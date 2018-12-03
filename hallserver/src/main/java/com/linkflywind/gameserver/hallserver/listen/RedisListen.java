f4ab70b0a828cf75c7cf47f1db99f74b60210382
qugang <qgass@163.com>
2018-12-03 19:12:07 +0800

    添加redis 的监听

--- hallserver/src/main/java/com/linkflywind/gameserver/hallserver/listen/RedisListen.java
+++ hallserver/src/main/java/com/linkflywind/gameserver/hallserver/listen/RedisListen.java
@@ -1,15 +1,9 @@
 package com.linkflywind.gameserver.hallserver.listen;
 
-import com.linkflywind.gameserver.core.TransferData;
-import com.linkflywind.gameserver.hallserver.GameServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
